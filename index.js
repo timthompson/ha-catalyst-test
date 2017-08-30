3812a52c3da85e39a7689c5b04e79770b8675536
timthompson <timothy.nathanial.thompson@gmail.com>
2017-08-30 17:06:17 -0500

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var timesqrt = require('./timesqrt');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.get('/simple', function(req, res){
+app.get('/simple', function(req, res) {
   res.send('Just another endpoint.');
+});
+
+app.get('/timesqrt', function(req, res) {
+   res.send(String(timesqrt())); 
 });
 
 console.log("Server is listening on port 3000");
---
+++ timesqrt.js
@@ -0,0 +1,4 @@
+module.exports = function () {
+    var dt = new Date();
+    return Math.sqrt(dt.getSeconds());
+};
